d2d7dc0b7009270da7fb8daab4e042f98e0945ff
Ankit-Basu <ankitbasu10@gmail.com>
2026-02-08 23:59:12 +0530

    minor changes

--- complaint-tracker/routes/complaint.routes.js
+++ complaint-tracker/routes/complaint.routes.js
@@ -11,7 +11,7 @@ import {
 const router = express.Router();
 
 router.get("/complaints", getComplaints);
-router.get("/complaints/export", exportComplaints); // Specific route before param route
+router.get("/complaints/export", exportComplaints);
 router.get("/complaints/:id", getComplaintById);
 router.post("/complaints", createComplaint);
 router.put("/complaints/:id", updateComplaintStatus);
